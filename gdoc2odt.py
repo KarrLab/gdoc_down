f1058d93f94e1542055c2969dcf918288865448b
Jonathan Karr <jonrkarr@gmail.com>
2016-06-18 16:51:32 -0400

    enabling output to other directories

--- gdoc2odt.py
+++ gdoc2odt.py
@@ -21,6 +21,7 @@ Last updated: 2015-11-03
 '''
 
 from apiclient import discovery
+import getopt
 import httplib2
 import json
 import re
@@ -68,6 +69,20 @@ def get_credentials():
     return credentials
 
 def main(argv):    
+    odir = '.'
+    try:
+        opts, args = getopt.getopt(argv, "he:o:", ["ext=", "odir"])
+    except getopt.GetoptError:
+        print('gdoc2odt.py -e <ext> -o <odir> <ifile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('gdoc2odt.py -e <ext> -o <odir> <ifile>')
+            sys.exit()
+        elif opt in ("-e", "--ext"):
+            ext = arg
+        elif opt in ("-o", "--odir"):
+            odir = arg
     ifile = argv[0]
     
     #authenticate
@@ -85,7 +100,7 @@ def main(argv):
     resp, content = service._http.request(file['exportLinks']['application/vnd.oasis.opendocument.text'])
      
     if resp.status == 200:
-        local_fd = open(ifile.replace('.gdoc', '.docx'), "wb")
+        local_fd = open(os.path.join(odir, ifile.replace('.gdoc', '.docx')), "wb")
         local_fd.write(content)
         local_fd.close()
         print('File saved')
