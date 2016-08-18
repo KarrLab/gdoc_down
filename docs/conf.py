f5f3efdd861e397b9b2a4dac0c2a316bad6a578a
Jonathan Karr <jonrkarr@gmail.com>
2016-08-18 16:29:30 -0400

    calling sphinx-apidoc from docs/conf.py for ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -357,3 +357,20 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+
+# -- Run sphinx-apidoc within ReadTheDocs on sphinx-build -----------------
+
+from configparser import ConfigParser
+from sphinx import apidoc
+
+def run_apidoc(app):
+    this_dir = os.path.dirname(__file__)
+    parser = ConfigParser()
+    parser.read(os.path.join(this_dir, '..', 'setup.cfg'))
+    packages = parser.get('sphinx-apidocs', 'packages').strip().split('\n')
+    for package in packages:
+        apidoc.main(argv=['sphinx-apidoc', '-f', '-o', os.path.join(this_dir, 'source'), os.path.join(this_dir, '..', package)])
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
--- docs/source/modules.rst
+++
@@ -1,7 +0,0 @@
-gdoc_down
-=========
-
-.. toctree::
-   :maxdepth: 4
-
-   gdoc_down
